79913c78735e60eed81220d71d76a39d71614a20
Maxim Soldatov <fomaxi@gmail.com>
2015-01-09 23:22:41 +0300

    update getprofiles

--- getprofiles.php
+++ getprofiles.php
@@ -12,12 +12,37 @@ rsort($authors);
 $start = time();
 $total = count($authors);
 foreach ($authors as $i => $author) {
+	if ($oldProfileRecord = $deviantartTop->db->profiles->findOne(['id' => $author])) {
+		if (isset($oldProfileRecord['server_deleted']) && $oldProfileRecord['server_deleted'] !== false)
+			continue;
+
+		if ($oldProfileRecord['local_updated'] > time() - Deviantart::$cache_time)
+			continue;
+	}
+
 	$line = "get profile: ".($i+1)."/$total $author";
 	echo "\r".str_pad($line, 80);
 
 	if ($profile = $deviantart->userinfo($author)) {
-		$profile['username'] = $author;
-		$deviantartTop->saveData('profiles', $author, $profile);
+		if (!isset($profile['deactivated'])) {
+			$profile['username'] = $author;
+			$deviantartTop->saveData('profiles', $author, $profile);
+		} else {
+			if ($oldProfileRecord) {
+				$deviantartTop->db->profiles->update(['id' => $author], ['$set' => [
+					'local_deleted' => false,
+					'server_deleted' => time(),
+				]]);
+			} else {
+				$deviantartTop->db->profiles->insert([
+					'id' => $author,
+					'local' => false,
+					'local_deleted' => false,
+					'server' => false,
+					'server_deleted' => time(),
+				]);
+			}
+		}
 	}
 }
 
